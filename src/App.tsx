908b2b2032c89152fc325b1f0322cac99dc74bd1
Chris Mulvihill <chris.mul@proton.me>
2025-08-20 19:46:02 +0100

    feature: using props to extract Item component

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,41 @@
-const list = [
-  {
-    title: "React",
-    url: "https://react.dev/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+type Story = {
+  title: string;
+  url: string;
+  author: string;
+  num_comments: number;
+  points: number;
+  objectID: number;
+};
 
-const App = () => (
-  <div>
-    <h1>My Road to React</h1>
-    <Search />
-    <hr />
-    <List />
-  </div>
-);
+const App = () => {
+  const stories: Story[] = [
+    {
+      title: "React",
+      url: "https://react.dev/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  return (
+    <div>
+      <h1>My Road to React</h1>
+      <Search />
+      <hr />
+      <List list={stories} />
+    </div>
+  );
+};
 
 const Search = () => {
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -42,19 +53,31 @@ const Search = () => {
   );
 };
 
-const List = () => (
+type ListProps = {
+  list: Story[];
+};
+
+const List = (props: ListProps) => (
   <ul>
-    {list.map((item) => (
-      <li key={item.objectID}>
-        <span>
-          <a href={item.url}>{item.title}</a>
-        </span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
-      </li>
+    {props.list.map((item) => (
+      <Item key={item.objectID} item={item} />
     ))}
   </ul>
+);
+
+type ItemProps = {
+  item: Story;
+};
+
+const Item = (props: ItemProps) => (
+  <li>
+    <span>
+      <a href={props.item.url}>{props.item.title}</a>
+    </span>
+    <span>{props.item.author}</span>
+    <span>{props.item.num_comments}</span>
+    <span>{props.item.points}</span>
+  </li>
 );
 
 export default App;
