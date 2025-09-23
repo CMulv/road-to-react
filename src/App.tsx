2d6dfaa9be1d7fbc0ec2104a6f94c0b8de15ca83
Chris Mulvihill <chris.mul@proton.me>
2025-09-23 19:54:05 +0100

    feature: using real hacker news api instead of pseudo data

--- src/App.tsx
+++ src/App.tsx
@@ -35,30 +35,6 @@ type StoriesAction =
   | StoriesFetchFailureAction
   | StoriesRemoveAction;
 
-const initialStories: Story[] = [
-  {
-    title: "React",
-    url: "https://react.dev/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
-const getAsyncStories = (): Promise<{ data: { stories: Story[] } }> =>
-  new Promise((resolve) =>
-    setTimeout(() => resolve({ data: { stories: initialStories } }), 2000)
-  );
-
 const storiesReducer = (state: StoriesState, action: StoriesAction) => {
   switch (action.type) {
     case "STORIES_FETCH_INIT":
@@ -104,6 +80,8 @@ const useStorageState = (key: string, initialState: string) => {
   return [value, setValue] as const;
 };
 
+const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query=";
+
 const App = () => {
   const [searchTerm, setSearchTerm] = useStorageState("search", "React");
   const [stories, dispatchStories] = React.useReducer(storiesReducer, {
@@ -114,11 +92,12 @@ const App = () => {
 
   React.useEffect(() => {
     dispatchStories({ type: "STORIES_FETCH_INIT" });
-    getAsyncStories()
+    fetch(`${API_ENDPOINT}react`)
+      .then((response) => response.json())
       .then((result) => {
         dispatchStories({
           type: "STORIES_FETCH_SUCCESS",
-          payload: result.data.stories,
+          payload: result.hits,
         });
       })
       .catch(() => dispatchStories({ type: "STORIES_FETCH_FAILURE" }));
